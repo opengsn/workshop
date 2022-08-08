7191d138b8bcde88c042a43d665fe6c60f74ecee
MRLoyalMafiaa247 <Futurepablo9@gmail.com>
2022-08-08 01:12:21 -0400

    Squashed commit of the following:
    commit d09a97430376f1ac0286cb175a60e06db6b9f927
    Author: Dror Tirosh <dror@opengsn.org>
    Date:   Mon Aug 1 14:45:52 2022 +0300
    
        With a custom paymaster
    
        This PR adds a custom paymaster to accept gas payments.

---
+++ contracts/ImportArtifacts.sol
@@ -0,0 +1,6 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+//"import" it into our project for Truffle to generate artifacts
+import "@opengsn/paymasters/contracts/WhitelistPaymaster.sol";
+
